32408b90de354ce147083f94f0cda8e83658c3cc
Danny Mösch <danny.moesch@icloud.com>
2025-11-18 14:01:37 +0100

    Extend `redundant_self_in_closure` to find all redundant `self`s

--- Source/SwiftLintCore/Visitors/DeclaredIdentifiersTrackingVisitor.swift
+++ Source/SwiftLintCore/Visitors/DeclaredIdentifiersTrackingVisitor.swift
@@ -132,6 +132,19 @@ open class DeclaredIdentifiersTrackingVisitor<Configuration: RuleConfiguration>:
         }
     }
 
+    override open func visit(_ node: FunctionDeclSyntax) -> SyntaxVisitorContinueKind {
+        if node.parent?.is(MemberBlockItemSyntax.self) != true {
+            scope.addToCurrentScope(.localVariable(name: node.name))
+        }
+        return .visitChildren
+    }
+
+    override open func visitPost(_ node: FunctionDeclSyntax) {
+        if node.parent?.is(MemberBlockItemSyntax.self) != true {
+            scope.pop()
+        }
+    }
+
     // MARK: Private methods
 
     private func collectIdentifiers(from parameters: FunctionParameterListSyntax) {
